8bd060889c870f6b23bdd169a8f9dc60a8be2fc8
Charles <594556046@qq.com>
2019-06-17 11:47:10 +0800

    Initial Commit!

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Charles Zhang
+ * @date : 2019-06-17 11:16
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("asda");
+    }
+
+}
