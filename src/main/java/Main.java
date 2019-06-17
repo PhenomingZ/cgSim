9d7d5037c3bee1a240afe4af81b090a3d71ad420
Charles Zhang <694556046@qq.com>
2019-06-17 13:31:24 +0800

    Add License, README and support Python programs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * @author : Charles Zhang
  * @date : 2019-06-17 11:16
@@ -6,7 +8,31 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("asda");
+
+        String exe = "python3 ";
+        String pyPath = "./src/main/python/__main__.py";
+
+        Process proc;
+
+        try {
+
+            proc = Runtime.getRuntime().exec(exe + pyPath);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+            String line = null;
+
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            in.close();
+            proc.waitFor();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
---
+++ src/main/python/__main__.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+if __name__ == '__main__':
+    a = np.arange(12).reshape(3, 4)
+    print(a)
